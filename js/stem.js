03f866ffa51cbf8ca827128b8cc92c4c61382ee2
Alfonso <127aramirez@frhsd.com>
2025-12-01 13:20:18 -0500

    done?

--- js/stem.js
+++ js/stem.js
@@ -1,11 +1,39 @@
-let Name = document.getElementById("name").value;
-let profileForm = document.querySelectorAll("profile").vaule;
-let ageInput = document.getElementById("age").vaule;
-let pet = document.querySelectorAll("Input[Type='radio']");
-let hobbies = document.querySelectorAll("Input[Type='checkbox']");
+let nameInput = document.getElementById("nameInput");
+let ageInput = document.getElementById("ageInput");
+let colorInput = document.getElementById("colorInput");
+let submitBtn = document.getElementById("submitBtn");
+let output = document.getElementById("output");
 
-SubmitEvent.addEventListner('click',getInfo);
+submitBtn.addEventListener("click", getInfo);
+
+
+let pet = document.querySelectorAll('input[type="radio"]');
+let hobby = document.querySelectorAll('input[type="checkbox"]');
+
+let favPet;
+let hobbies = [];
 
 function getInfo(){
+    // reset hobbies Array each time value
+    hobbies = [];
+
     
-}
+    //check for pets
+    for(let i = 0; i < pet.length; i++){
+        if(pet[i].checked){
+            favPet = pet[i].value;
+        }
+    }
+    console.log(favPet);
+    
+    //cehck hobbies
+    for(let i = 0; i < hobby.length; i++){
+        if(hobby[i].checked){
+            hobbies.push(" " + hobby[i].value);
+        }
+    }
+    console.log(hobbies);
+
+    output.textContent = `Name: ${nameInput.value}, Age: ${ageInput.value}, Favorite Color: ${colorInput.value}, Pet: ${petInput.value}, Hobby: ${hobbyInput.value}`;
+    
+} 
