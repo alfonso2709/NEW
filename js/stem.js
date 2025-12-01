66b6f0418500e49d24639e34c6c25d56c2bf6b2e
Alfonso <127aramirez@frhsd.com>
2025-12-01 13:15:16 -0500

    done

---
+++ js/stem.js
@@ -0,0 +1,11 @@
+let Name = document.getElementById("name").value;
+let profileForm = document.querySelectorAll("profile").vaule;
+let ageInput = document.getElementById("age").vaule;
+let pet = document.querySelectorAll("Input[Type='radio']");
+let hobbies = document.querySelectorAll("Input[Type='checkbox']");
+
+SubmitEvent.addEventListner('click',getInfo);
+
+function getInfo(){
+    
+}
